b48f5d5cf847b14a1c4bd0e45809c00f2bd8c2cf
yash-vij <yashvij1996@gmail.com>
2022-11-17 22:15:59 +0530

    Multiple data MongoDB

--- FetchFromMongoDB.py
+++ FetchFromMongoDB.py
@@ -1,12 +1,15 @@
-import pandas as pd
 import pymongo
-df = pd.read_csv("bank/bank.csv",sep=";")
-print(df.head())
-df.reset_index(inplace=True)
-mongoData = df.to_dict("records")
+import pandas as pd
 client = pymongo.MongoClient("mongodb+srv://yashvij:0000@cluster0.wnq7l.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 db1 = client["MultipleMongoData"]
 collectionsMultiple = db1['CollectionMultipleData']
-collectionsMultiple.insert_many(mongoData)
+## Retriving data From Mongo DB
 
+data_find = collectionsMultiple.find({
+    'age':{"$gt":20}
+})
+
+
+df1 = pd.DataFrame(data_find)
+df1.to_excel("MongoData.xlsx")
