32bebc7745abb485e839bfb1105ddd97af54bb53
yash-vij <yashvij1996@gmail.com>
2022-11-17 20:44:34 +0530

    python msql

---
+++ mongoDBConnection.py
@@ -0,0 +1,14 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://yashvij:0000@cluster0.wnq7l.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+data = {
+    "name":"Yash",
+    "ID":101,
+    "Address" : "Delhi"
+}
+db1 = client['Mongotest11']
+collection1 = db1['coll1']
+collection1.insert_one(data)
